c7e075c0cbdcd93272489dc720bad9b3fcc79998
amirihusayn <amirgoodarzi75@gmail.com>
2022-02-25 15:28:24 +0330

    Update events
    - Update avatar path
    - Add loading
    - Refactor capture.js

---
+++ assets/js/capture.js
@@ -0,0 +1,46 @@
+const postElement = document.querySelector("#capture-post");
+const storyElement = document.querySelector("#capture-story");
+const saveContainer = document.querySelector("#event-image-container");
+const loadingElement = document.querySelector("#loading");
+
+// Capture post element
+html2canvas(postElement, {
+  width: 1080,
+  height: 1080,
+  scale: 1,
+  windowWidth: 1080,
+  windowHeight: 1080
+}).then(canvas => {
+  const imageURL = canvas.toDataURL('image/png').replace('image/png', 'image/octet-stream');
+  const aElement = document.createElement('a');
+  saveContainer.appendChild(canvas);
+  postElement.remove();
+  aElement.setAttribute('download', 'post-image.png');
+  aElement.setAttribute('class', 'my-button');
+  aElement.setAttribute('href', imageURL);
+  aElement.innerText = "دانلود تصویر مناسب پست";
+  canvas.remove();
+  saveContainer.appendChild(aElement);
+});
+
+// Capture story element
+html2canvas(storyElement, {
+  width: 1080,
+  height: 1920,
+  scale: 1,
+  windowWidth: 1080,
+  windowHeight: 1920
+}).then(canvas => {
+  const imageURL = canvas.toDataURL('image/png').replace('image/png', 'image/octet-stream');
+  const aElement = document.createElement('a');
+  saveContainer.appendChild(canvas);
+  storyElement.remove();
+  aElement.setAttribute('download', 'story-image.png');
+  aElement.setAttribute('class', 'my-button');
+  aElement.setAttribute('href', imageURL);
+  aElement.innerText = "دانلود تصویر مناسب استوری";
+  canvas.remove();
+  saveContainer.appendChild(aElement);
+  // Remove loading spinner
+  setTimeout(loadingElement.style.display = 'none', 1000);
+});
