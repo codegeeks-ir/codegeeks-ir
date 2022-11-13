9be98ba9b06e773b808716901021c266cbb57a65
amirihusayn <amirgoodarzi75@gmail.com>
2022-11-14 02:11:11 +0330

    Update pdf export of events
    - Improve quality of pdf export ( 2x dpi )
    - Fix some style problems

--- assets/js/export-event.js
+++ assets/js/export-event.js
@@ -27,14 +27,14 @@ const storyOptions = {
 const pdfOptions = {
   width: 793.3,
   height: 1122,
-  scale: 1,
+  scale: 2,
   windowWidth: 793.3,
   windowHeight: 1122
 };
 const html2pdfOptions = {
   filename:     'myfile.pdf',
   image:        { type: 'jpeg', quality: 0.98 },
-  html2canvas:  { scale: 1 },
+  html2canvas:  { scale: 2 },
   jsPDF:        { unit: 'in', format: 'A4', orientation: 'portrait' }
 };
 
