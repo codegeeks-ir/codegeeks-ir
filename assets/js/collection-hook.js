35a3673fb7b72e1b1735a955f448fd6ab056b53d
amirihusayn <amirgoodarzi75@gmail.com>
2023-02-01 01:50:27 +0330

    Add property based search option for collections
    - Update json data generators for all collections

--- assets/js/collection-hook.js
+++ assets/js/collection-hook.js
@@ -1,51 +1,58 @@
+function getNumberOfPages(pageSize, collectionLength) {
+    if( collectionLength % pageSize == 0)
+        return collectionLength / pageSize; 
+    else
+        return Math.floor(collectionLength / pageSize) + 1; 
+}
+
+function filterSearch(collection, searchProperty, search) {
+    if(collection) {
+        const filteredByProperty = collection.filter((item) => {
+            let isFound = item[searchProperty].toLowerCase().includes(search.toLowerCase());
+            if(isFound)
+                return true;
+        });
+        return filteredByProperty;
+    }
+    else return [];
+}
+
+function paged(collection, currentPage, pageSize) {
+    if(collection) {
+        const paged = collection.filter((item, index) => {
+            let startIndex = (currentPage - 1) * pageSize;
+            let endIndex = currentPage * pageSize;
+            let isItemInsidePage = startIndex <= index && index < endIndex;
+            if(isItemInsidePage)
+                return true;
+        });
+        return paged;
+    }
+    else return [];
+}
+
 document.addEventListener('alpine:init', () => {
     Alpine.data('collection', () => ({
         collection: [],
+        collectionProperties: [],
         filtered: [],
         search: "",
+        searchProperty: "",
         currentPage: 1,
         pageSize: 5,
         pageCount:0,
-        getNumberOfPages(pageSize, collectionLength) {
-            if( collectionLength % pageSize == 0)
-                return collectionLength / pageSize; 
-            else
-                return Math.floor(collectionLength / pageSize) + 1; 
-        },
-        filterSearch(collection, property) {
-            if(collection) {
-                const filteredByProperty = collection.filter((item) => {
-                    let isFound = item[property].toLowerCase().includes(this.search.toLowerCase());
-                    if(isFound)
-                        return true;
-                });
-                this.pageCount = this.getNumberOfPages(this.pageSize, filteredByProperty.length);
-                return filteredByProperty;
-            }
-            else return [];
-        },
-        paged(collection, currentPage, pageSize) {
-            if(collection) {
-                const paged = collection.filter((item, index) => {
-                    let startIndex = (currentPage - 1) * pageSize;
-                    let endIndex = currentPage * pageSize;
-                    let isItemInsidePage = startIndex <= index && index < endIndex;
-                    if(isItemInsidePage)
-                        return true;
-                });
-                return paged;
-            }
-            else return [];
-        },
         viewPage(pageIndex) {
-            const property = 'title';
-            const searched = this.filterSearch(this.collection, property);
+            const searched = filterSearch(this.collection, this.searchProperty, this.search);
+            this.pageCount = getNumberOfPages(this.pageSize, searched.length);
             this.currentPage = pageIndex;
-            this.filtered = this.paged(searched, pageIndex, this.pageSize);
+            this.filtered = paged(searched, pageIndex, this.pageSize);
         },
         async init() {
             const dataPath = document.getElementById('#collection').getAttribute('dataPath');
-            this.collection = await (await fetch(dataPath)).json();
+            const data = await (await fetch(dataPath)).json();
+            this.collection = data.data;
+            this.collectionProperties = data.properties;
+            this.searchProperty = Object.keys(this.collectionProperties)[0];
             this.viewPage(1);
         }
     }));
