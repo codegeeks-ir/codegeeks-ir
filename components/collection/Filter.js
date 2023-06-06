ca832c0edbbd35186eaf839e2a297e75676e0005
amirihusayn <amirgoodarzi75@gmail.com>
2023-06-07 02:33:19 +0330

    Feat: Make collection filter responsive

--- components/collection/Filter.js
+++ components/collection/Filter.js
@@ -1,7 +1,7 @@
 import FilterIcon from "public/icones/filter.svg";
 import { useMemo, useRef, useState } from "react";
 
-function filterSearch(propCollection, searchProperty, search) {
+const filterSearch = (propCollection, searchProperty, search) => {
   if (propCollection) {
     const resultsByProperty = propCollection.filter((item) => {
       let isFound = item[searchProperty]
@@ -11,85 +11,80 @@ function filterSearch(propCollection, searchProperty, search) {
     });
     return resultsByProperty;
   } else return [];
-}
+};
 
-function FilterOptions({ propCollection, mySearch, setMySearch }) {
-  // function preventDefaultSearch(event) {
-  //   if (event.key == "/") event.preventDefault();
-  // }
-
-  // function changeSearchProperty(newProperty) {
-  //   searchProperty.current = newProperty;
-  // }
-
-  // onKeyDown={[
-  //   (e) => this.preventDefaultSearch(e),
-  //   search.current.focus(),
-  // ]}
-  return (
-    <div className="flex flex-row flex-wrap">
-      <input
-        type="search"
-        placeholder="جستجو"
-        className="form-element"
-        onChange={(e) =>
-          setMySearch({
-            ...mySearch,
-            searchInput: e.target.value,
-            results: filterSearch(
-              propCollection,
-              mySearch.searchProperty,
-              e.target.value
-            ),
-          })
-        }
-      />
-      <div className="flex flex-row flex-wrap">
-        {mySearch.properties.map((property) => {
-          if (property.searchable)
-            return (
-              <button
-                className={`mx-0.5 ${
-                  mySearch.searchProperty == property.enName
-                    ? "btn-primary"
-                    : "btn-light"
-                }`}
-                onClick={() =>
-                  setMySearch({ ...mySearch, searchProperty: property.enName })
-                }
-                key={property.enName}
-              >
-                {property.faName}
-              </button>
-            );
-        })}
-      </div>
+const FilterOptions = ({
+  propCollection,
+  mySearch,
+  setMySearch,
+  showFilters,
+}) => (
+  <div
+    className={`flex flex-row flex-wrap items-center grow 
+    ${showFilters ? "visible" : "invisible"}`}
+  >
+    <input
+      type="search"
+      placeholder="جستجو"
+      className="form-element"
+      onChange={(e) =>
+        setMySearch({
+          ...mySearch,
+          searchInput: e.target.value,
+          results: filterSearch(
+            propCollection,
+            mySearch.searchProperty,
+            e.target.value
+          ),
+        })
+      }
+    />
+    <div className="flex flex-row flex-wrap items-center grow">
+      {mySearch.properties.map((property) => {
+        if (property.searchable)
+          return (
+            <button
+              className={`mx-0.5 grow md:grow-0 ${
+                mySearch.searchProperty == property.enName
+                  ? "btn-primary"
+                  : "btn-light"
+              }`}
+              onClick={() =>
+                setMySearch({ ...mySearch, searchProperty: property.enName })
+              }
+              key={property.enName}
+            >
+              {property.faName}
+            </button>
+          );
+      })}
     </div>
-  );
-}
+  </div>
+);
 
-export default function Filter({ propCollection, mySearch, setMySearch }) {
+const Filter = ({ propCollection, mySearch, setMySearch }) => {
   const [showFilters, setShowFilters] = useState(true);
   return (
     <div
       className="card flex flex-col w-full justify-start items-start
             rounded-t-none mt-0 py-3 px-2 lg:px-5 md:px-4 sm:px-2"
     >
-      <div className="flex flex-row flex-wrap w-full">
+      <div className="flex flex-row md:flex-row-reverse flex-wrap w-full">
+        <FilterOptions
+          propCollection={propCollection}
+          mySearch={mySearch}
+          setMySearch={setMySearch}
+          showFilters={showFilters}
+        />
         <button
-          className="btn-primary"
+          className="btn-primary flex w-full md:w-9 h-9 items-center justify-center"
           onClick={() => setShowFilters(!showFilters)}
         >
-          <FilterIcon className="icon" />
+          <FilterIcon className="fill-slate-200 w-6" />
         </button>
-        {showFilters ? (
-          <FilterOptions
-            propCollection={propCollection}
-            mySearch={mySearch}
-            setMySearch={setMySearch}
-          />
-        ) : null}
       </div>
     </div>
   );
-}
+};
+
+export default Filter;
