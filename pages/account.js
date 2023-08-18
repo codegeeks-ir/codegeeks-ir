58fcbc5c83e75cefb0c8afeed2d4a4c2a8014469
amirihusayn <amirgoodarzi75@gmail.com>
2023-08-19 00:17:57 +0330

    Initial profile page

---
+++ pages/account.js
@@ -0,0 +1,28 @@
+import Tabs from "components/Tabs.js";
+import EventForm from "components/collection/form/EventForm";
+import DefaultLayout from "layouts/DefaultLayout";
+import ChallengeForm from "components/collection/form/ChallengeForm";
+import ProjectForm from "components/collection/form/ProjectForm";
+import BlogForm from "components/collection/form/BlogForm";
+import Login from "components/Login";
+import LogoType from "components/LogoType";
+
+const Account = () => (
+  <div className="mt-4 flex h-full max-h-screen w-full flex-col items-center">
+    <LogoType />
+    <Login />
+    <Tabs
+      headers={["ایجاد رویداد", "ایجاد مسابقه", "ایجاد پروژه", "ایجاد پست"]}
+      contents={[
+        <EventForm />,
+        <ChallengeForm />,
+        <ProjectForm />,
+        <BlogForm />,
+      ]}
+    />
+  </div>
+);
+
+Account.getLayout = (content) => <DefaultLayout>{content}</DefaultLayout>;
+
+export default Account;
