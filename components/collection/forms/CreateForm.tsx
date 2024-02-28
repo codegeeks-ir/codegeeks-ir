aabd189cebf90c9659ae7b0d14369248c87630bc
amirihusayn <amirgoodarzi75@gmail.com>
2024-02-28 16:59:40 -0500

    hotfix: comment components/collection/forms/CreateForm.tsx

--- components/collection/forms/CreateForm.tsx
+++ components/collection/forms/CreateForm.tsx
@@ -1,36 +1,36 @@
-"use client";
-import useFormables from "hooks/useFormables";
-import { Format } from "utils/schema/data";
-import FormFactory from "components/properties/form";
+// "use client";
+// import useFormables from "hooks/useFormables";
+// import { Format } from "utils/schema/data";
+// import FormFactory from "components/properties/form";
 
-interface IProps {
-  format: Format;
-}
+// interface IProps {
+//   format: Format;
+// }
 
-const Form = ({ format }: IProps) => {
-  const formables = useFormables(format);
-  return (
-    <form className="form">
-      <div className="flex w-full flex-row flex-wrap">
-        <div className="flex flex-col flex-wrap">
-          {formables.map((item) => (
-            <FormFactory
-              property={item.property}
-              local={item.local!}
-              key={item.local?.global.en.name}
-            />
-          ))}
-        </div>
-      </div>
-      <a
-        className="btn-primary mx-0 w-full md:w-48"
-        href={`${config.login}`}
-        onClick={() => createEvent({})}
-      >
-        ایجاد
-      </a>
-    </form>
-  );
-};
+// const Form = ({ format }: IProps) => {
+//   const formables = useFormables(format);
+//   return (
+//     <form className="form">
+//       <div className="flex w-full flex-row flex-wrap">
+//         <div className="flex flex-col flex-wrap">
+//           {formables.map((item) => (
+//             <FormFactory
+//               property={item.property}
+//               local={item.local!}
+//               key={item.local?.global.en.name}
+//             />
+//           ))}
+//         </div>
+//       </div>
+//       <a
+//         className="btn-primary mx-0 w-full md:w-48"
+//         href={`${config.login}`}
+//         onClick={() => createEvent({})}
+//       >
+//         ایجاد
+//       </a>
+//     </form>
+//   );
+// };
 
-export default Form;
+// export default Form;
